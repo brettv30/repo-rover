2bf120e44df01665fcbdfecacf80afed41a4d4f6
brettv30 <brettvogelsang30@gmail.com>
2024-07-26 23:06:04 -0400

    Update models pulled from ollama

--- pull-ollama-models.py
+++ pull-ollama-models.py
@@ -37,7 +37,11 @@ def pull_model_with_retry(model_name, max_retries=5, delay=60):
 
 
 # List of models to pull
-models = ["phi3:3.8b", "llama3.1:8b"]
+models = [
+    "phi3:3.8b-mini-128k-instruct-q3_K_S",
+    "llama3.1:8b-instruct-q2_K",
+    "nomic-embed-text",
+]
 
 # Pull the models from the Ollama server if not already available
 for model in models:
