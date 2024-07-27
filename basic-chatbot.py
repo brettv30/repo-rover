5787b1029ee9e0d92f32024b7cdca993f0bff1df
brettv30 <brettvogelsang30@gmail.com>
2024-07-26 23:05:56 -0400

    Set up basic plan-execute agent workflow

--- basic-chatbot.py
+++ basic-chatbot.py
@@ -1,32 +1,110 @@
 import os
+import asyncio
 from dotenv import load_dotenv
 from langchain.agents import Tool
 from langchain_community.tools import DuckDuckGoSearchRun
 from langchain_community.agent_toolkits.github.toolkit import GitHubToolkit
 from langchain_community.utilities.github import GitHubAPIWrapper
 from langchain import hub
+from langchain_core.prompts import ChatPromptTemplate
 from langgraph.prebuilt import create_react_agent
-from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama, OllamaEmbeddings
 from langchain_community.vectorstores import Qdrant
 from qdrant_client import QdrantClient, models
 from langchain_community.document_loaders import (
-    DirectoryLoader,
     GitLoader,
 )
+from langgraph.graph import StateGraph, START
 from langchain_nomic import NomicEmbeddings
+import operator
+from langchain_core.pydantic_v1 import BaseModel, Field
+from typing import Annotated, List, Tuple, TypedDict, Union, Literal
 
-load_dotenv()
+# load_dotenv()
+
+
+class PlanExecute(TypedDict):
+    input: str
+    plan: List[str]
+    past_steps: Annotated[List[Tuple], operator.add]
+    response: str
+
+
+class Plan(BaseModel):
+    """Plan to follow in future"""
+
+    steps: List[str] = Field(
+        description="different steps to follow, should be in sorted order"
+    )
+
+
+class Response(BaseModel):
+    """Response to user."""
+
+    response: str
+
+
+class Act(BaseModel):
+    """Action to perform."""
+
+    action: Union[Response, Plan] = Field(
+        description="Action to perform. If you want to respond to user, use Response. "
+        "If you need to further use tools to get the answer, use Plan."
+    )
+
+
+async def execute_step(state: PlanExecute):
+    executor = set_execution_agent()
+    plan = state["plan"]
+    plan_str = "\n".join(f"{i+1}. {step}" for i, step in enumerate(plan))
+    task = plan[0]
+    task_formatted = f"""For the following plan:
+{plan_str}\n\nYou are tasked with executing step {1}, {task}."""
+    agent_response = await executor.ainvoke({"messages": [("user", task_formatted)]})
+    return {
+        "past_steps": (task, agent_response["messages"][-1].content),
+    }
+
+
+async def plan_step(state: PlanExecute):
+    planner = set_planner_agent()
+    plan = await planner.ainvoke({"messages": [("user", state["input"])]})
+    return {"plan": plan.steps}
+
+
+async def replan_step(state: PlanExecute):
+    replanner = set_replanner_agent()
+    output = await replanner.ainvoke(state)
+    if isinstance(output.action, Response):
+        return {"response": output.action.response}
+    else:
+        return {"plan": output.action.steps}
+
+
+def should_end(state: PlanExecute) -> Literal["agent", "__end__"]:
+    if "response" in state and state["response"]:
+        return "__end__"
+    else:
+        return "agent"
+
+
+def read_secret(secret_name):
+    path = os.path.join("/run/secrets", secret_name)
+    if os.path.exists(path):
+        with open(path, "r") as secret_file:
+            return secret_file.read().strip()
+    return None
 
 
 def set_environment_variables():
-    os.environ["LANGCHAIN_TRACING_V2"] = os.getenv("LANGCHAIN_TRACING_V2")
-    os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
-    os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
-    os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-    os.environ["NOMIC_API_KEY"] = os.getenv("NOMIC_API_KEY")
-    os.environ["GITHUB_APP_ID"] = os.getenv("GITHUB_APP_ID")
-    os.environ["GITHUB_APP_PRIVATE_KEY"] = os.getenv("GITHUB_APP_PRIVATE_KEY")
-    os.environ["GITHUB_BRANCH"] = "repo-rover-branch"
+    os.environ["LANGCHAIN_API_KEY"] = read_secret("langchain_api_key")
+    # os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+    # os.environ["NOMIC_API_KEY"] = os.getenv("NOMIC_API_KEY")
+    # os.environ["GITHUB_APP_ID"] = int(read_secret("github_app_id"))
+    os.environ["GITHUB_APP_PRIVATE_KEY"] = read_secret("github_app_private_key")
+    os.environ["GITHUB_REPOSITORY"] = read_secret("github_repo")
+    os.environ["ACTIVE_BRANCH"] = "repo-rover-branch"
+    os.environ["GITHUB_BASE_BRANCH"] = "main"
 
 
 def set_github_repository(repo_link):
@@ -99,11 +177,9 @@ def index_in_qdrant(embeddings, qdrant_client, collection_name):
 
 
 def set_embeddings_model():
-    model = NomicEmbeddings(
-        model="nomic-embed-text-v1.5", inference_mode="local", device="gpu"
-    )
+    embedder = OllamaEmbeddings(model="nomic-embed-text")
 
-    return model
+    return embedder
 
 
 def load_and_index_files(repo_link, repo_path):
@@ -128,7 +204,13 @@ def load_and_index_files(repo_link, repo_path):
 
 def set_agent_tools():
     # GitHub Toolkit
-    github = GitHubAPIWrapper()
+    github = GitHubAPIWrapper(
+        github_app_id=int(read_secret("github_app_id")),
+        github_app_private_key=os.getenv("GITHUB_APP_PRIVATE_KEY"),
+        active_branch=os.getenv("ACTIVE_BRANCH"),
+        github_base_branch=os.getenv("GITHUB_BASE_BRANCH"),
+        github_repository=os.getenv("GITHUB_REPOSITORY"),
+    )
     gh_toolkit = GitHubToolkit.from_github_api_wrapper(github)
     gh_tools = gh_toolkit.get_tools()
 
@@ -144,39 +226,109 @@ def set_agent_tools():
     return tools
 
 
-def set_execution_agent(tools):
-    prompt = hub.pull("repo-rover-execution-agent-prompt")
+def set_planner_agent():
+    planner_prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                """For the given objective, come up with a simple step by step plan. \
+    This plan should involve individual tasks, that if executed correctly will yield the correct answer. Do not add any superfluous steps. \
+    The result of the final step should be the final answer. Make sure that each step has all the information needed - do not skip steps.""",
+            ),
+            ("placeholder", "{messages}"),
+        ]
+    )
+    planner = planner_prompt | ChatOllama(
+        model="llama3.1:8b-instruct-q2_K",
+        temperature=0,
+        num_gpu=1,
+        verbose=True,
+    ).with_structured_output(Plan)
+
+    return planner
+
+
+def set_replanner_agent():
+    replanner_prompt = ChatPromptTemplate.from_template(
+        """For the given objective, come up with a simple step by step plan. \
+    This plan should involve individual tasks, that if executed correctly will yield the correct answer. Do not add any superfluous steps. \
+    The result of the final step should be the final answer. Make sure that each step has all the information needed - do not skip steps.
+
+    Your objective was this:
+    {input}
+
+    Your original plan was this:
+    {plan}
+
+    You have currently done the follow steps:
+    {past_steps}
+
+    Update your plan accordingly. If no more steps are needed and you can return to the user, then respond with that. Otherwise, fill out the plan. Only add steps to the plan that still NEED to be done. Do not return previously done steps as part of the plan."""
+    )
+
+    replanner = replanner_prompt | ChatOllama(
+        model="llama3.1:8b-instruct-q2_K",
+        temperature=0,
+        num_gpu=1,
+        verbose=True,
+    ).with_structured_output(Act)
+
+    return replanner
+
+
+def set_execution_agent():
+    prompt = hub.pull("wfh/react-agent-executor")
 
     # Choose the LLM that will drive the agent
-    llm = ChatOpenAI(model="gpt-3.5-turbo")
-    agent_executor = create_react_agent(llm, tools, messages_modifier=prompt)
-    return agent_executor
-
-
-def main():
-    set_environment_variables()
-    repo_link = input("Enter the GitHub URL of the repository:")
-    branch = input("Enter the name of the base branch:")
-
-    set_github_repository(repo_link)
-    set_github_base_branch(branch)
-
-    new_temp_path = "/app/temp-repo"
-    # new_temp_path = (
-    #     "C:\\Users\\Brett\\OneDrive\\Desktop\\firearm-research-team\\temp-repo"
-    # )
-    if os.path.exists(new_temp_path):
-        print(f"{new_temp_path} already exists.")
-    else:
-        os.mkdir(new_temp_path)
-
-    load_and_index_files(repo_link, new_temp_path)
-
-    print("Files loaded and indexed successfully.")
+    llm = ChatOllama(
+        model="phi3:3.8b-mini-128k-instruct-q3_K_S", num_gpu=1, verbose=True
+    )
     tools = set_agent_tools()
-    print("Tools loaded successfully.")
-    # agent_executor = set_execution_agent(tools)
+    executor = create_react_agent(llm, tools, messages_modifier=prompt)
+    return executor
+
+
+async def main():
+    set_environment_variables()
+    print("Set Environment Variables")
+
+    workflow = StateGraph(PlanExecute)
+
+    # Add the plan node
+    workflow.add_node("planner", plan_step)
+
+    # Add the execution step
+    workflow.add_node("agent", execute_step)
+
+    # Add a replan node
+    workflow.add_node("replan", replan_step)
+
+    workflow.add_edge(START, "planner")
+
+    # From plan we go to agent
+    workflow.add_edge("planner", "agent")
+
+    # From agent, we replan
+    workflow.add_edge("agent", "replan")
+
+    workflow.add_conditional_edges(
+        "replan",
+        # Next, we pass in the function that will determine which node is called next.
+        should_end,
+    )
+
+    # Finally, we compile it!
+    # This compiles it into a LangChain Runnable,
+    # meaning you can use it as you would any other runnable
+    app = workflow.compile()
+
+    config = {"recursion_limit": 50}
+    inputs = {"input": "What GitHub repository do you have access to?"}
+    async for event in app.astream(inputs, config=config):
+        for k, v in event.items():
+            if k != "__end__":
+                print(v)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
